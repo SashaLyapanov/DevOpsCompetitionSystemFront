881d9f69d0f181452e4f64d1b7961bd665302156
SashaLyapanov <lyapanov_s@mail.ru>
2025-04-03 11:53:55 +0300

    Change url config for localhost.

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'http://localhost:8080/api';    //Для локалки
-const API_BASE_URL = '/api';    //Для Docker
+const API_BASE_URL = 'http://localhost:8080/api';    //Для локалки
+// const API_BASE_URL = '/api';    //Для Docker
 
 // Компетиции
 export const fetchCompetitions = () => {
